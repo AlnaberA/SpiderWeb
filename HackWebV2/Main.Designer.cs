8372c28f51dca6eb51e52f051da24e43f2d57d8f
alnaberalaa <alnaberalaa@gmail.com>
2021-02-23 20:36:48 -0500

    init commit

--- HackWebV2/Main.Designer.cs
+++ HackWebV2/Main.Designer.cs
@@ -30,12 +30,17 @@ namespace HackWebV2
         private void InitializeComponent()
         {
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.forwardBtn = new System.Windows.Forms.Button();
+            this.url = new System.Windows.Forms.TextBox();
+            this.geckoWebBrowser = new Gecko.GeckoWebBrowser();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,14 +51,76 @@ namespace HackWebV2
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel.ColumnCount = 1;
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.geckoWebBrowser, 0, 1);
             this.tableLayoutPanel.Location = new System.Drawing.Point(4, 25);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 2;
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.417722F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 96.58228F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(1238, 790);
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.443038F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.55696F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(1241, 790);
             this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1117F));
+            this.tableLayoutPanel1.Controls.Add(this.backBtn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.forwardBtn, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.url, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1235, 37);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // backBtn
+            // 
+            this.backBtn.Image = global::HackWebV2.Properties.Resources.back_32px;
+            this.backBtn.Location = new System.Drawing.Point(3, 3);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(50, 30);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // forwardBtn
+            // 
+            this.forwardBtn.Image = global::HackWebV2.Properties.Resources.forward_32px;
+            this.forwardBtn.Location = new System.Drawing.Point(66, 3);
+            this.forwardBtn.Name = "forwardBtn";
+            this.forwardBtn.Size = new System.Drawing.Size(49, 30);
+            this.forwardBtn.TabIndex = 1;
+            this.forwardBtn.UseVisualStyleBackColor = true;
+            this.forwardBtn.Click += new System.EventHandler(this.forwardBtn_Click);
+            // 
+            // url
+            // 
+            this.url.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.url.Location = new System.Drawing.Point(121, 3);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(1111, 20);
+            this.url.TabIndex = 2;
+            this.url.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlEntry_KeyPress);
+            // 
+            // geckoWebBrowser
+            // 
+            this.geckoWebBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.geckoWebBrowser.ConsoleMessageEventReceivesConsoleLogCalls = true;
+            this.geckoWebBrowser.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser.Location = new System.Drawing.Point(3, 46);
+            this.geckoWebBrowser.Name = "geckoWebBrowser";
+            this.geckoWebBrowser.Size = new System.Drawing.Size(1235, 741);
+            this.geckoWebBrowser.TabIndex = 1;
+            this.geckoWebBrowser.UseHttpActivityObserver = false;
+            this.geckoWebBrowser.DocumentCompleted += new System.EventHandler<Gecko.Events.GeckoDocumentCompletedEventArgs>(this.geckoWebBrowser_DocumentCompleted);
             // 
             // menuStrip1
             // 
@@ -83,17 +150,8 @@ namespace HackWebV2
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
             this.aboutToolStripMenuItem.Text = "About";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 20);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "backBtn";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
@@ -106,6 +164,8 @@ namespace HackWebV2
             this.Name = "Main";
             this.Text = "HackWeb";
             this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -120,7 +180,11 @@ namespace HackWebV2
         private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Button forwardBtn;
+        private System.Windows.Forms.TextBox url;
+        private Gecko.GeckoWebBrowser geckoWebBrowser;
     }
 }
 
--- HackWebV2/Main.cs
+++ HackWebV2/Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gecko;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,30 @@ namespace HackWebV2
         public Main()
         {
             InitializeComponent();
+            Xpcom.Initialize("Firefox");
+        }
+
+        private void urlEntry_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                geckoWebBrowser.Navigate(url.Text);
+            }
+        }
+
+        private void geckoWebBrowser_DocumentCompleted(object sender, Gecko.Events.GeckoDocumentCompletedEventArgs e)
+        {
+            url.Text = geckoWebBrowser.Url.AbsoluteUri;
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser.GoBack();
+        }
+
+        private void forwardBtn_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser.GoForward();
         }
     }
 }
