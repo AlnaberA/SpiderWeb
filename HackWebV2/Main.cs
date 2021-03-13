9a581188b889faf179fd28b1f59b655c3e3998a8
alnaberalaa <alnaberalaa@gmail.com>
2021-03-12 20:48:37 -0500

    adding test code root cert code.

--- HackWebV2/Main.cs
+++ HackWebV2/Main.cs
@@ -1,4 +1,5 @@
-﻿using Gecko;
+﻿using Fiddler;
+using Gecko;
 using HackWebV2.HelpMenu;
 using HackWebV2.Menu_Tools;
 using System;
@@ -7,6 +8,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -63,36 +66,70 @@ namespace HackWebV2
 
         private void setupProxy()
         {
-            /*            GeckoPreferences.Default["network.proxy.type"] = 1;
-                        GeckoPreferences.Default["network.proxy.http"] = proxyAddress.Host;
-                        GeckoPreferences.Default["network.proxy.http_port"] = proxyAddress.Port;
-                        GeckoPreferences.Default["network.proxy.ssl"] = proxyAddress.Host;
-                        GeckoPreferences.Default["network.proxy.ssl_port"] = proxyAddress.Port;*/
-        }
+            GeckoPreferences.Default["network.proxy.type"] = 1;
+            GeckoPreferences.Default["network.proxy.http"] = "127.0.0.1";
+            GeckoPreferences.Default["network.proxy.http_port"] = 8764;
+            GeckoPreferences.Default["network.proxy.https"] = "127.0.0.1";
+            GeckoPreferences.Default["network.proxy.https_port"] = 8764;
+            GeckoPreferences.Default["network.proxy.ssl"] = "127.0.0.1";
+            GeckoPreferences.Default["network.proxy.ssl_port"] = 8764;
 
+            GeckoPreferences.User["security.enterprise_roots.enabled"] = true;
+            GeckoPreferences.User["security.enterprise_roots.auto-enabled"] = true;
+        }
 
         private void testMethod()
         {
             System.Diagnostics.Trace.WriteLine("testing.");
-
-            // Consume the dom changed event.
-            geckoWebBrowser.DomContentChanged += (sender, args) =>
+            /*           
+             *           
+             *          https://stackoverflow.com/questions/17904789/using-fiddler-with-a-c-sharp-application-ipport-proxy
+                        https://stackoverflow.com/questions/21029531/listen-to-browsers-requests
+                        https://stackoverflow.com/questions/22150216/how-to-check-single-url-webtraffic-in-c-sharp
+                        https://stackoverflow.com/questions/19238425/geckofx-22-by-pass-self-sign-cert
+            */
+            /*            geckoWebBrowser.NSSError += (s, e) =>
+                        {
+                            if (e.Message.Contains("Certificate"))//Peer's Certificate issuer is not recognized.
+                            {
+                                CertOverrideService.GetService().RememberValidityOverride(e.Uri, e.Certificate, CertOverride.Mismatch | CertOverride.Time | CertOverride.Untrusted, false);
+                                if (!e.Uri.AbsoluteUri.Contains(".js") && !e.Uri.AbsoluteUri.Contains(".css")) geckoWebBrowser.Navigate(e.Uri.AbsoluteUri);
+                                e.Handled = true;//otherwise shows error
+                            }
+                        };*/
+            String cert = "";
+            String key = "";
+            if (!CertMaker.rootCertExists())
             {
-                System.Diagnostics.Trace.WriteLine("Dom has changed.");
-            };
+                bool flag = false;
+                if (!CertMaker.createRootCert())
+                    flag = false;
 
-            // Consume the DomClick event.
-            geckoWebBrowser.DomClick += (sender, args) =>
-            {
-                System.Diagnostics.Trace.WriteLine("Dom clicked event called.");
-            };
+                if (!CertMaker.trustRootCert())
+                    flag = false;
 
-            geckoWebBrowser.UseHttpActivityObserver = true;
-            geckoWebBrowser.ObserveHttpModifyRequest += (sender, args) =>
-            {
-                System.Diagnostics.Trace.WriteLine("ObserveHttpModifyRequest event called.");
-            };
-            //https://stackoverflow.com/questions/22150216/how-to-check-single-url-webtraffic-in-c-sharp
+                // persist Fiddlers certificate into app specific config
+                cert = FiddlerApplication.Prefs.GetStringPref("fiddler.certmaker.bc.cert", null);
+                key = FiddlerApplication.Prefs.GetStringPref("fiddler.certmaker.bc.key", null);
+            }
+
+            FiddlerApplication.Prefs.SetStringPref("fiddler.certmaker.bc.key", key);
+            FiddlerApplication.Prefs.SetStringPref("fiddler.certmaker.bc.cert", cert);
+
+            Fiddler.FiddlerApplication.BeforeRequest += FiddlerApplication_BeforeRequest;
+            Fiddler.FiddlerApplication.Startup(8764, false, true);
+
+
+        }
+
+        private void FiddlerApplication_BeforeRequest(Fiddler.Session oSession)
+        {
+            System.Diagnostics.Trace.WriteLine(String.Format("REQ: {0}", oSession.url));
+        }
+
+        public void Dispose()
+        {
+            Fiddler.FiddlerApplication.Shutdown();
         }
     }
 }
